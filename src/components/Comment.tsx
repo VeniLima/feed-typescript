0e1e434180d8ec49a427f8253e240ffbd38f4390
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2022-06-30 08:32:32 -0300

    html comentarios

---
+++ src/components/Comment.tsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "../styles/Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="http://github.com/venilima.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vinicius Lima</strong>
+              <time
+                className={styles.time}
+                title="14 de Maio as 8 horas"
+                dateTime="2022-05-14 08:00:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Post.module.css";
+import { Comment } from "./Comment";
 
 export function Post() {
   return (
@@ -46,6 +47,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
